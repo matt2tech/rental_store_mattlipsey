4a31587989184f038de817cdd6cff077b450acf8
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-24 15:05:26 -0500

    created test for returning function

--- test_core.py
+++ test_core.py
@@ -22,3 +22,16 @@ def test_renting_in_stock():
     renting('1', inv)
 
     assert inv['1']['In-Stock'] == 4
+
+
+def test_returning():
+    inv = {
+        '1': {
+            'Item': 'Grill',
+            'Rental Rate': 5.00,
+            'Replacement Value': 300.00,
+            'In-Stock': 5
+        }
+    }
+    returning('1', inv)
+    assert inv['1']['In-Stock'] == 6
