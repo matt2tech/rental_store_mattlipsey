4168b104d35ec472acba1d1d10a61f2c8935f114
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-24 14:28:31 -0500

    created functions to convert history.txt into dictionaries in shell

--- disk.py
+++ disk.py
@@ -1,14 +1,14 @@
 import core
 
 
-def file_contents():
+def inventory_contents():
     with open('inventory.txt', 'r') as file:
         file.readline()
         contents = file.read()
     return contents
 
 
-def file_stock_conversion(contents):
+def inventory_stock_conversion(contents):
     item = contents.split(',')
     item_2 = '{0:.2f}'.format(float(item[2]))
     item_3 = '{0:.2f}'.format(float(item[3]))
@@ -16,12 +16,12 @@ def file_stock_conversion(contents):
 
 
 def load_inventory():
-    contents = file_contents()
+    contents = inventory_contents()
     inventory = {}
     lines = contents.split('\n')
     for line in lines:
         if line:
-            item = file_stock_conversion(line)
+            item = inventory_stock_conversion(line)
             inventory[item[0]] = {
                 'Item': item[1],
                 'Rent Per Day': item[2],
@@ -35,7 +35,7 @@ def load_inventory():
 def print_inventory(inventory):
     for item, info in inventory.items():
         print(
-            "Item Number: {}\n\tItem: {}\n\tRent Per Day: {}\n\tReplacement Value: {}\n\tIn-Stock: {}\n".
+            "Item Number: {}\n\tItem: {}\n\tRent Per Day: ${}\n\tReplacement Value: ${}\n\tIn-Stock: {}\n".
             format(item, info['Item'], info['Rent Per Day'],
                    info['Replacement Value'], info['In-Stock']))
 
@@ -59,3 +59,38 @@ def update_stock(inventory):
     text = convert_to_string(inventory)
     with open('inventory.txt', 'w') as file:
         file.write(text)
+
+
+def transcations_contents():
+    with open('history.txt', 'r') as file:
+        file.readline()
+        contents = file.read()
+    return contents
+
+
+def transcations_stock_conversion(contents):
+    item = contents.split(',')
+    item_3 = '{0:.2f}'.format(float(item[3]))
+    return item[0], item[1], int(item[2]), item_3
+
+
+def load_transcations():
+    contents = transcations_contents()
+    transcations = {}
+    lines = contents.split('\n')
+    for line in lines:
+        if line:
+            item = transcations_stock_conversion(line)
+            transcations[item[0]] = {
+                'Item': item[1],
+                'Days Rented': item[2],
+                'Total': item[3]
+            }
+
+    return transcations
+
+
+def print_transcations(transcations):
+    for item, info in transcations.items():
+        print("Name: {}\n\tItem: {}\n\tDays Rented: {}\n\tTotal: ${}".format(
+            item, info['Item'], info['Days Rented'], info['Total']))
