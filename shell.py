ae1581558af27e5364c09747453b7fc13ae9a1c8
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-24 12:40:52 -0500

    Moved functions that dealt with inventory.txt from shell to disk. Now working on converting inventory back into file to update inventory.txt

--- shell.py
+++ shell.py
@@ -2,45 +2,6 @@ import disk
 import core
 
 
-def file_contents():
-    with open('inventory.txt', 'r') as file:
-        file.readline()
-        contents = file.read()
-    return contents
-
-
-def file_stock_conversion(contents):
-    item = contents.split(',')
-    item_2 = '{0:.2f}'.format(float(item[2]))
-    item_3 = '{0:.2f}'.format(float(item[3]))
-    return (item[0]), item[1], item_2, item_3, int(item[4])
-
-
-def load_inventory():
-    contents = file_contents()
-    inventory = {}
-    lines = contents.split('\n')
-    for line in lines:
-        if line:
-            item = file_stock_conversion(line)
-            inventory[item[0]] = {
-                'Item': item[1],
-                'Rent Per Day': item[2],
-                'Replacement Value': item[3],
-                'In-Stock': item[4]
-            }
-
-    return inventory
-
-
-def print_inventory(inventory):
-    for item, info in inventory.items():
-        print(
-            "Item Number: {}\n\tItem: {}\n\tRent Per Day: {}\n\tReplacement Value: {}\n\tIn-Stock: {}\n".
-            format(item, info['Item'], info['Rent Per Day'],
-                   info['Replacement Value'], info['In-Stock']))
-
-
 def main():
     inventory = load_inventory()
     user = ''
@@ -49,6 +10,7 @@ def main():
             'Welcome to Matt\'s Rents.\nAre you a customer or employee?\n1 - Customer\n2 - Employee\n3 - Quit\n>>> '
         )
         if user == '1':
+            name = input('What is the name of this order?\n>>> ')
             option = ''
             while option != '3':
                 option = input(
