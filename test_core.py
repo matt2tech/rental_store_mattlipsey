3d4b35b250ca5c0de50bef7b270c728e637dc653
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-24 15:02:21 -0500

    created tests for renting function

---
+++ test_core.py
@@ -0,0 +1,24 @@
+from core import *
+from bcca.test import should_print
+
+
+@should_print
+def test_renting_out_of_stock(output):
+    renting('1', {'1': {'In-Stock': 0}})
+
+    assert output == 'Out of stock.'
+
+
+def test_renting_in_stock():
+    inv = {
+        '1': {
+            'Item': 'Grill',
+            'Rental Rate': 5.00,
+            'Replacement Value': 300.00,
+            'In-Stock': 5
+        }
+    }
+
+    renting('1', inv)
+
+    assert inv['1']['In-Stock'] == 4
