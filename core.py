b2a86fa73e9f0a6a0ebb88763ea627671ba9bc2e
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-26 11:14:58 -0500

    created a return_listing function that helps return_total

--- core.py
+++ core.py
@@ -23,8 +23,31 @@ def replacement_deposit(cart, inventory):
     return total
 
 
-def return_fee(cart, inventory, days):
+def return_fee(inv):
     total = 0
-    for item in cart:
-        total += round((inventory[item]['Rental Rate'] * 1.07), 2) * days
+    for item, info in inv.items():
+        total += round(
+            (float(info['Rental Rate'])), 2) * float(info['Days Rented'])
     return total
+
+
+def deposit_return(inv):
+    total = 0
+    for item, info in inv.items():
+        total += float(info['Replacement Deposited'])
+    return total
+
+
+def return_listing(return_item, inventory, days):
+    return_dict = {}
+    item_name = inventory[return_item]['Item']
+    rental_rate = '{0:.2f}'.format(
+        inventory[return_item]['Rental Rate'] * 1.07)
+    replacement = '{0:.2f}'.format(
+        inventory[return_item]['Replacement Value'] * .1)
+    return_dict[item_name] = {
+        'Days Rented': days,
+        'Rental Rate': rental_rate,
+        'Replacement Deposited': replacement
+    }
+    return return_dict
