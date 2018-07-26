2ffe6bd1753f0a34b9ed43c9f8c931b3548e4b08
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-26 09:46:04 -0500

    created a return_fee function for return_total in shell

--- core.py
+++ core.py
@@ -12,12 +12,19 @@ def renting(item_number, inventory):
 def rental_fee(cart, inventory):
     total = 0
     for item in cart:
-        total += inventory[item]['Rental Rate'] * 1.07
-    return round(total, 2)
+        total += round(inventory[item]['Rental Rate'] * 1.07, 2)
+    return total
 
 
-def replacement_fee(cart, inventory):
+def replacement_deposit(cart, inventory):
     total = 0
     for item in cart:
-        total += inventory[item]['Replacement Value'] * .1
-    return round(total, 2)
+        total += round(inventory[item]['Replacement Value'] * .1, 2)
+    return total
+
+
+def return_fee(cart, inventory, days):
+    total = 0
+    for item in cart:
+        total += round((inventory[item]['Rental Rate'] * 1.07), 2) * days
+    return total
