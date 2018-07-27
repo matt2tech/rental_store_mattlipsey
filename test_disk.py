772b97aee03efc2e34e6dda18cf9e03848be1c0e
matt2tech <mlipsey@basecampcodingacademy.org>
2018-07-27 09:33:48 -0500

    Created a test for inventory_stock_conversion

--- test_disk.py
+++ test_disk.py
@@ -14,4 +14,10 @@ def test_inventory_contents():
     assert inventory_contents() == 'line\nline\nline\n'
 
 
-# def test_inventory_stock_conversion():
+def test_inventory_stock_conversion():
+
+    contents = '1,Grill,3.0,80.0,5'
+
+    content = inventory_stock_conversion(contents)
+
+    assert content == ('1', 'Grill', '3.0', '80.0', 5)
